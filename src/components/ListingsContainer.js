88bb85cfbbd26fdaa056b7a812f561fd8505472c
David Doan <daviddoan@Davids-MacBook-Pro.local>
2023-08-29 12:51:59 -0600

    solution

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,10 +1,41 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, {useState,useEffect} from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({search}) {
+  const [listings, setListings] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:6001/listings")
+    .then(r=>r.json())
+    .then(data=>setListings(data))
+  },[])
+  // console.log(listings)
+  const filterData = listings.filter((listing)=>{
+    return listing.description.toLowerCase().includes(search.toLowerCase())
+  })
+  console.log(filterData)
+  const listingsCards = filterData.map((listing)=>{
+    return <ListingCard key={listing.id} listing={listing} deleteById={deleteById}/>
+  })
+
+  function deleteById(id){
+    fetch(`http://localhost:6001/listings/${id}`,{method:"DELETE"})
+    .then(r=>r.json())
+    .then(data=>{
+      const newListingArray = listings.filter((listing)=>{
+        if(id===listing.id){
+          return false
+        }
+        return true
+      })
+      setListings(newListingArray)
+    })
+  }
+
   return (
     <main>
       <ul className="cards">
+        {listingsCards}
         {/* use the ListingCard component to display listings */}
       </ul>
     </main>
